8079edaf3979f0ed0e8d0871f387e43778d57c18
Bar Snir <bars@Admins-MacBook-Pro.local>
2020-05-13 12:40:06 +0300

    update fetcher tag

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
-tag=1.0.10
+tag=1.1.0
 
 docker build -t 572995054717.dkr.ecr.eu-west-1.amazonaws.com/y2_elastic_fetcher:$tag .
 docker push 572995054717.dkr.ecr.eu-west-1.amazonaws.com/y2_elastic_fetcher:$tag
