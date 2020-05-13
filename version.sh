14904b06baa8a683a6bbaa4168a246b7a9a7ea10
Bar Snir <bars@Admins-MacBook-Pro.local>
2020-05-13 12:20:36 +0300

    added shell for docker build

---
+++ version.sh
@@ -0,0 +1,4 @@
+tag=1.0.10
+
+docker build -t 572995054717.dkr.ecr.eu-west-1.amazonaws.com/y2_elastic_fetcher:$tag .
+docker push 572995054717.dkr.ecr.eu-west-1.amazonaws.com/y2_elastic_fetcher:$tag
